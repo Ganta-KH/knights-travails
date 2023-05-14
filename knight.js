cad8c5d65eca9cf83245d53b4fd0ca63f6714dea
Khalil <Khalilbentaiba@gmail.com>
2023-05-14 23:28:57 +0100

    add board for the knight

--- knight.js
+++ knight.js
@@ -1,6 +1,11 @@
+import Board from "./board.js";
+
 export default class Knight {
-    constructor(start) {
+    constructor(start, x = 8, y = 8) {
         this.start = start;
+        this.x = x;
+        this.y = y;
+        this.board = new Board(x, y)
     }
 
     getMoves(pos = this.start) {
@@ -8,22 +13,22 @@ export default class Knight {
         // top top
         if (pos[0] - 2 > -1) {
             if (pos[1] - 1 >= 0) arr.push([pos[0] - 2, pos[1] - 1]);
-            if (pos[1] + 1 < 8) arr.push([pos[0] - 2, pos[1] + 1]);
+            if (pos[1] + 1 < this.y) arr.push([pos[0] - 2, pos[1] + 1]);
         }
         // top bottom
         if (pos[0] - 1 > -1) {
             if (pos[1] - 2 >= 0) arr.push([pos[0] - 1, pos[1] - 2]);
-            if (pos[1] + 2 < 8) arr.push([pos[0] - 1, pos[1] + 2]);
+            if (pos[1] + 2 < this.y) arr.push([pos[0] - 1, pos[1] + 2]);
         }
         // bottom top
-        if (pos[0] + 1 <= 7) {
+        if (pos[0] + 1 < this.x) {
             if (pos[1] - 2 > -1) arr.push([pos[0] + 1, pos[1] - 2]);
-            if (pos[1] + 2 < 8) arr.push([pos[0] + 1, pos[1] + 2]);
+            if (pos[1] + 2 < this.y) arr.push([pos[0] + 1, pos[1] + 2]);
         }
         // bottom bottom
-        if (pos[0] + 2 < 8) {
+        if (pos[0] + 2 < this.x) {
             if (pos[1] - 1 > -1) arr.push([pos[0] + 2, pos[1] - 1]);
-            if (pos[1] + 1 < 8) arr.push([pos[0] + 2, pos[1] + 1]);
+            if (pos[1] + 1 < this.y) arr.push([pos[0] + 2, pos[1] + 1]);
         }
         return arr;
     }
@@ -38,7 +43,7 @@ export default class Knight {
             if (end.toString() === start.toString())
                 return this.path(this.start, end, road);
             const moves = this.getMoves(start);
-            for (let move of moves) {
+            for (const move of moves) {
                 if (!visited.has(move.toString())) {
                     queue.push(move);
                     visited.add(move.toString());
@@ -51,17 +56,15 @@ export default class Knight {
 
     path(start, end, nodes) {
         const path = [];
-        console.log(nodes);
         path.push(end);
         while (end.toString() !== start.toString()) {
             end = nodes[end.toString()];
             path.push(end);
         }
         path.reverse();
+
+        // set positions in the board
+        this.board.setValues(path)
         return path;
     }
 }
-
-const knight = new Knight([0, 0]);
-
-console.log(knight.bfs([1, 7]));
